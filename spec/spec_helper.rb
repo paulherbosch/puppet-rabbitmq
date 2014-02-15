af0237b7df736f1f374c8e314853b5d07179dfbc
Kristof Willaert <kristof.willaert@cegeka.be>
2014-02-15 23:39:03 +0100

    Fixtures scaffolding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,15 @@
-require 'puppet'
 require 'rspec-puppet'
-require 'tmpdir'
+require 'mocha'
 
 def verify_contents(subject, type, title, expected_lines)
   content = subject.resource(type, title).send(:parameters)[:content]
   (content.split("\n") & expected_lines).should == expected_lines
 end
 
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
 RSpec.configure do |c|
-  c.before :each do
-    # Create a temporary puppet confdir area and temporary site.pp so
-    # when rspec-puppet runs we don't get a puppet error.
-    @puppetdir = Dir.mktmpdir
-    manifestdir = File.join(@puppetdir, "manifests")
-    Dir.mkdir(manifestdir)
-    FileUtils.touch(File.join(manifestdir, "site.pp"))
-    Puppet[:confdir] = @puppetdir
-  end
-
-  c.after :each do
-    FileUtils.remove_entry_secure(@puppetdir)
-  end
-
-  c.module_path = File.join(File.dirname(__FILE__), 'fixtures/modules')
+  c.mock_with :mocha
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 end
-
